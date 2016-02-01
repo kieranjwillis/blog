9bd04b13a0bcb2c353ae6bf2b25e5b07cb86271a
ali97 <m.abdullah.ali@gmx.ch>
2016-02-01 08:45:41 +0100

    Created index

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include("header.php");?>
+
+		<div class="container piccontainer" id="mountains">
+			<h2 class="pictitle">Welcome.</h2>
+			<div class="picitem">
+				<h3>Blog</h3>
+    				<a href="login.php"><button type="submit">LOGIN</button></a><br>
+    				<a href="register.php"><button type="submit">REGISTER</button></a>
+			</div>
+		</div>
+	</body>
+</html>
